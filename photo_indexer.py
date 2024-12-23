75d923b91274cd59372b7f9845b5bebc2de63b70
itisacrouton <itisacrouton@proton.me>
2024-12-23 20:46:54 +1100

    added try excepts in case photos dont have appropriate metadata

--- photo_indexer.py
+++ photo_indexer.py
@@ -9,27 +9,44 @@ out_folder = "static/photos/"
 
 manifest = {}
 
+all_photos = []
+
 sum_photos = 0
 for folder in os.listdir(dcim_dir):
     sum_photos += len(os.listdir(dcim_dir+folder))
 
-current_photo_index = 0
 for folder in os.listdir(dcim_dir):
     for photo in os.listdir(dcim_dir+folder):
-        dst_file = out_folder+f'{current_photo_index}.jpg'
         src_file = dcim_dir+folder+'/'+photo
-
+        print(src_file)
         img = Image.open(src_file)
         img_exif = img.getexif()
-        datetime_str = img_exif[ExifTags.Base.DateTime]
+        try:
+            datetime_str = img_exif[ExifTags.Base.DateTime]
+        except KeyError:
+            continue
         datetime_obj = datetime.datetime.strptime(datetime_str, '%Y:%m:%d %H:%M:%S')
         datetime_formatted = datetime.datetime.strftime(datetime_obj, '%A, %d %b, %Y at %X')
-        caption = img_exif[ExifTags.Base.ImageDescription]
+        epoch = datetime_obj.timestamp()
+        try:
+            caption = img_exif[ExifTags.Base.ImageDescription]
+        except KeyError:
+            caption = ''
         img.close()
 
-        manifest[str(current_photo_index)] = {'src': folder+'/'+photo, 'datetime': datetime_formatted, 'caption': caption}
-        shutil.copy2(src_file, dst_file)
-        current_photo_index += 1
+        all_photos.append({'src': src_file, 'caption': caption, 'datetime': datetime_formatted, 'epoch': epoch})
+
+all_photos = sorted(all_photos, key = lambda x: x['epoch'])
+
+current_photo_index = 0
+for current_photo_index, photo in enumerate(all_photos):
+    dst_file = out_folder+f'{current_photo_index}.jpg'
+    src_file = photo['src']
+
+    manifest[str(current_photo_index)] = {'src': photo['src'], 'datetime': photo['datetime'], 'caption': photo['caption']}
+    shutil.copy2(src_file, dst_file)
+    current_photo_index += 1
+    print(current_photo_index)
 
 print(sum_photos)
 
